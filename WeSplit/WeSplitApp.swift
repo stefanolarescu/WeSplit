51fc3f9ab860de6985eaefd5acad53017d428620
Stefan Olarescu <stefanolarescu@gmail.com>
2024-09-12 16:32:40 +0300

    Created Xcode project.

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Stefan Olarescu on 12.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
